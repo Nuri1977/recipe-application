e02ed87a0135285013608390c9e804c504685dca
Nuri1977 <nurilacka@gmail.com>
2022-05-17 11:19:28 +0200

    Implement adding new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,25 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe.id), notice: 'recipe was successfully created!'
+    else
+      redirect_to new_recipe_path, notice: 'recipe could not be created!'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :user_id, :preparation_time, :cooking_time, :description, :public)
+  end
+
 end
